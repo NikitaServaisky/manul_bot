a40ecdd4e2c821b348ad32328bf727089438d082
Nikita <servaiski@gmail.com>
2026-04-08 16:47:24 +0300

    new file geting db

--- core/database.py
+++ core/database.py
@@ -3,7 +3,6 @@ import sqlite3
 
 def get_db():
     """connect to data base function"""
-    db_path = "/app/manul_leads.db"
-    conn = sqlite3.connect("manul_leads.db")
+    conn = sqlite3.connect("/app/manul_leads.db")
     conn.row_factory = sqlite3.Row
     return conn
