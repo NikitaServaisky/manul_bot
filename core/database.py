fdcb6412661a7c0eea01ecd0dda83befc27c0875
Nikita <servaiski@gmail.com>
2026-04-08 16:38:16 +0300

    add full path to DB file

--- core/database.py
+++ core/database.py
@@ -3,6 +3,7 @@ import sqlite3
 
 def get_db():
     """connect to data base function"""
+    db_path = "/app/manul_leads.db"
     conn = sqlite3.connect("manul_leads.db")
     conn.row_factory = sqlite3.Row
     return conn
