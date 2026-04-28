bb6a25d56104dcfa1c42d78d79fb3722c76c5507
Nikita <servaiski@gmail.com>
2026-04-29 00:21:34 +0300

    321

--- core/setup/get_schema_files.py
+++ core/setup/get_schema_files.py
@@ -1,17 +1,14 @@
 import os
 
-
 def get_schema_files(schema_path="schema"):
-    """Scaned folder of schems and return sort SQL"""
+    """
+    Scans the schema directory for .sql files and returns them sorted.
+    """
     if not os.path.exists(schema_path):
-        print(f"Warning: Schema folder '{schema_path}' not found.")
         return []
 
-    # Search only files with .sql
-    files = [f for f in os.listdir(schema_path) if f.endswitch(".sql")]
-
-    # Sorting about alpabet
-    firls.sort()
-
-    # Returned full path of the file
+    files = [f for f in os.listdir(schema_path) if f.endswith(".sql")]
+    files.sort()
+    
+    # Return full paths
     return [os.path.join(schema_path, f) for f in files]
